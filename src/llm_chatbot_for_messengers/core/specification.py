bc1904a21d3fa45a4c347e529bf6daf2b4d987db
sad-zero <zeroro.yun@gmail.com>
2024-11-27 10:48:11 +0900

    Add timeout specification

--- src/llm_chatbot_for_messengers/core/specification.py
+++ src/llm_chatbot_for_messengers/core/specification.py
@@ -4,7 +4,12 @@ Domain Specification
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Callable
+import inspect
+import time
+from functools import partial, wraps
+from typing import TYPE_CHECKING, Any, Callable
+
+from llm_chatbot_for_messengers.core.error import SpecificationError
 
 if TYPE_CHECKING:
     from llm_chatbot_for_messengers.core.vo import WorkflowNodeConfig
@@ -31,3 +36,54 @@ def check_necessary_nodes(
         return workflow_configs
 
     return _check
+
+
+def check_timeout(func: Callable[..., Any], *, timeout: int) -> Callable[..., Any]:
+    """Check agent/node timeout
+    Args:
+        func (Callable[..., Any]): Target function
+        timeout             (int): Timeout Seconds
+    Returns:
+        Callable[..., Any]: Traced target function
+    """
+    if not isinstance(timeout, int):
+        err_msg: str = 'timeout should be integer value(seconds)'
+        raise TypeError(err_msg)
+    if timeout <= 0:
+        err_msg = 'timeout should be greater than 0'
+        raise ValueError(err_msg)
+
+    if func is None:
+        return partial(check_timeout, timeout=timeout)
+
+    if not inspect.isfunction(func):
+        err_msg = 'func should be function'
+        raise TypeError(err_msg)
+
+    err_msg_template: str = '{elapsed: .2f} is longer than {timeout}'
+
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        s: float = time.time()
+        result: Any = func(*args, **kwargs)
+        e: float = time.time()
+        elapsed: float = e - s
+        if elapsed >= timeout:
+            err_msg = err_msg_template.format(elapsed=elapsed, timeout=timeout)
+            raise SpecificationError(err_msg)
+        return result
+
+    @wraps(func)
+    async def awrapper(*args, **kwargs) -> Any:
+        s: float = time.time()
+        result: Any = await func(*args, **kwargs)
+        e: float = time.time()
+        elapsed: float = e - s
+        if elapsed >= timeout:
+            err_msg = err_msg_template.format(elapsed=elapsed, timeout=timeout)
+            raise SpecificationError(err_msg)
+        return result
+
+    if inspect.iscoroutinefunction(func):
+        return awrapper
+    return wrapper
---
+++ tests/core/test_specification.py
@@ -0,0 +1,53 @@
+import asyncio
+import time
+
+import pytest
+from llm_chatbot_for_messengers.core.error import SpecificationError
+from llm_chatbot_for_messengers.core.specification import check_timeout
+
+
+def _normal_func(elapsed: int):
+    time.sleep(elapsed)
+    return True
+
+
+async def _async_func(elapsed: int):
+    await asyncio.sleep(elapsed)
+    return True
+
+
+@pytest.mark.parametrize(
+    ('elapsed', 'timeout', 'expected'),
+    [
+        (1.7, 2, 'ok'),
+        (2, 2, 'error'),
+        (2.1, 2, 'error'),
+    ],
+)
+def test_check_timeout_normal(elapsed, timeout, expected):
+    traced = check_timeout(func=_normal_func, timeout=timeout)
+    match expected:
+        case 'ok':
+            assert traced(elapsed)
+        case 'error':
+            with pytest.raises(SpecificationError):
+                assert traced(elapsed)
+
+
+@pytest.mark.parametrize(
+    ('elapsed', 'timeout', 'expected'),
+    [
+        (1.7, 2, 'ok'),
+        (2, 2, 'error'),
+        (2.1, 2, 'error'),
+    ],
+)
+@pytest.mark.asyncio
+async def test_check_timeout_async(elapsed, timeout, expected):
+    traced = check_timeout(func=_async_func, timeout=timeout)
+    match expected:
+        case 'ok':
+            assert await traced(elapsed)
+        case 'error':
+            with pytest.raises(SpecificationError):
+                assert await traced(elapsed)
