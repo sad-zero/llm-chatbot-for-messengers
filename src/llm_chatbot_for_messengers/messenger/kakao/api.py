6eea44a83e96b8649df1663e10a67c92b2e5fb43
sad-zero <zeroro.yun@gmail.com>
2024-12-04 17:12:14 +0900

    IoC Container

--- src/llm_chatbot_for_messengers/messenger/kakao/api.py
+++ src/llm_chatbot_for_messengers/messenger/kakao/api.py
@@ -4,7 +4,7 @@ from typing import Annotated
 from fastapi import Depends, FastAPI
 
 from llm_chatbot_for_messengers.core.entity.agent import QAAgent
-from llm_chatbot_for_messengers.messenger.kakao.container import get_qa_agent
+from llm_chatbot_for_messengers.messenger.kakao.container import get_qa_agent, init_containers
 from llm_chatbot_for_messengers.messenger.kakao.vo import ChatRequest, ChatResponse, ChatTemplate, SimpleTextOutput
 
 logging.basicConfig(level=logging.DEBUG)
@@ -12,8 +12,10 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 app = FastAPI(title='Kakao LLM Chatbot')
 
+init_containers()
 
-@app.post('/kakao/v1/chat')
+
+@app.post('/kakao/v1/chat', response_model=None)
 async def chat(body: ChatRequest, qa_agent: Annotated[QAAgent, Depends(get_qa_agent)]) -> ChatResponse:
     user = body.userRequest.user.to()
     answer = await qa_agent.ask(user=user, question=body.userRequest.utterance, timeout=4)
--- src/llm_chatbot_for_messengers/messenger/kakao/container.py
+++ src/llm_chatbot_for_messengers/messenger/kakao/container.py
@@ -1,28 +1,45 @@
-from typing import Generator
+from dependency_injector import containers, providers
+from dependency_injector.wiring import Provide, inject
 
 from llm_chatbot_for_messengers.core.entity.agent import QAAgent, QAAgentImpl
 from llm_chatbot_for_messengers.core.output.memory import VolatileMemoryManager
 from llm_chatbot_for_messengers.core.vo import LLMConfig, WorkflowGlobalConfig, WorkflowNodeConfig
 
 
-def get_qa_agent() -> Generator[QAAgent, None, None]:
+class AgentContainer(containers.DeclarativeContainer):
+    qa_agent: providers.Singleton[QAAgent] = providers.Singleton(
+        QAAgentImpl,
+        workflow_configs={
+            'answer_node': WorkflowNodeConfig(
+                node_name='answer_node',
+                template_name='kakao_v1',
+                llm_config=LLMConfig(model='gpt-4o-2024-08-06', temperature=0.52, max_tokens=200),
+            )
+        },
+        global_configs=WorkflowGlobalConfig(
+            fallback_message='미안해용. ㅠㅠ 질문이 너무 어려워용..', memory_manager=VolatileMemoryManager()
+        ),
+    )
+
+
+def init_containers():
+    agent_container = AgentContainer()
+    agent_container.check_dependencies()
+    agent_container.wire(
+        modules=[
+            'llm_chatbot_for_messengers.messenger.kakao.container',
+        ]
+    )
+
+
+@inject
+def _get_qa_agent(agent: QAAgent = Provide[AgentContainer.qa_agent]) -> QAAgent:
     """
     Returns:
         QAAgent: Fully initialized instance
     """
-    try:
-        agent = QAAgentImpl.get_instance(
-            workflow_configs={
-                'answer_node': WorkflowNodeConfig(
-                    node_name='answer_node',
-                    template_name='kakao_v1',
-                    llm_config=LLMConfig(model='gpt-4o-2024-08-06', temperature=0.52, max_tokens=200),
-                )
-            },
-            global_configs=WorkflowGlobalConfig(
-                fallback_message='미안해용. ㅠㅠ 질문이 너무 어려워용..', memory_manager=VolatileMemoryManager()
-            ),
-        )
-        yield agent
-    finally:
-        ...
+    return agent
+
+
+def get_qa_agent() -> QAAgent:
+    return _get_qa_agent()
