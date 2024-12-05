a334446a993b0403350b058b43c84bfff634000a
sad-zero <zeroro.yun@gmail.com>
2024-12-05 16:08:51 +0900

    Persistant Memory

--- tests/core/test_memory.py
+++ tests/core/test_memory.py
@@ -1,11 +1,29 @@
+import os
+
 import pytest
-from llm_chatbot_for_messengers.core.output.memory import MemoryManager, MemoryType, VolatileMemoryManager
+from llm_chatbot_for_messengers.core.output.memory import (
+    MemoryManager,
+    MemoryType,
+    PersistantMemoryManager,
+    VolatileMemoryManager,
+)
 
 
 @pytest.mark.asyncio
 async def test_volatile_memory():
     # given
     manager: MemoryManager = VolatileMemoryManager()
+    # when
+    memory: MemoryType = await manager.acquire_memory()
+    # then
+    assert isinstance(memory, MemoryType)
+    await manager.release_memory()
+
+
+@pytest.mark.asyncio
+async def test_persistant_memory():
+    # given
+    manager: MemoryManager = PersistantMemoryManager(conn_uri=os.getenv('CORE_DB_URI'))  # type: ignore
     # when
     memory: MemoryType = await manager.acquire_memory()
     # then
