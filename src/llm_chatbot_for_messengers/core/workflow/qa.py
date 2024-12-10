3dd00935728c8f30448e876e428230084ffbecf8
sad-zero <zeroro.yun@gmail.com>
2024-12-10 17:18:51 +0900

    Move node to class

--- src/llm_chatbot_for_messengers/core/workflow/qa.py
+++ src/llm_chatbot_for_messengers/core/workflow/qa.py
@@ -17,46 +17,6 @@ if TYPE_CHECKING:
     from llm_chatbot_for_messengers.core.output.memory import MemoryType
 
 
-async def answer_node(state: QAState, llm: BaseChatModel, template_name: str | None = None) -> QAState:
-    """Make a final answer.
-
-    Args:
-        state            (QAState): {
-            "question": ...,
-            "messages": ...,
-        }
-        llm        (BaseChatModel): LLM for answer node
-        template_name (str | None): Prompt Template name
-    Returns:
-        QAState: {
-            "answer": ...
-        }
-    """
-    if llm is None:
-        error_msg: str = 'LLM is not passed!'
-        raise RuntimeError(error_msg)
-
-    template = get_template(node_name='answer_node', template_name=template_name).partial(
-        messages=state.get_formatted_messages()
-    )
-    try:
-        chain: Runnable = (
-            {'question': RunnablePassthrough()}
-            | template
-            | llm.with_structured_output(AnswerNodeResponse, method='json_schema')
-        )
-    except NotImplementedError:
-        chain = (
-            {'question': RunnablePassthrough()}
-            | template
-            | llm
-            | PydanticOutputParser(pydantic_object=AnswerNodeResponse)
-        )
-
-    answer: AnswerNodeResponse = await chain.ainvoke(state.question)
-    return QAState.put_answer(answer=answer.answer)
-
-
 class QAWorkflow(Workflow[QAState]):
     @classmethod
     @override
@@ -64,7 +24,9 @@ class QAWorkflow(Workflow[QAState]):
         if (answer_node_config := config.get('answer_node')) is None:
             answer_node_config = WorkflowNodeConfig(node_name='answer_node')
         answer_node_llm = cls._build_llm(llm_config=answer_node_config.llm_config)
-        answer_node_with_llm = partial(answer_node, llm=answer_node_llm, template_name=answer_node_config.template_name)
+        answer_node_with_llm = partial(
+            cls.__answer_node, llm=answer_node_llm, template_name=answer_node_config.template_name
+        )
 
         graph = (
             cls._graph_builder(state_schema=QAState)
@@ -74,3 +36,43 @@ class QAWorkflow(Workflow[QAState]):
             .compile(checkpointer=memory)
         )
         return cls(compiled_graph=graph, state_schema=QAState)
+
+    @staticmethod
+    async def __answer_node(state: QAState, llm: BaseChatModel, template_name: str | None = None) -> QAState:
+        """Make a final answer.
+
+        Args:
+            state            (QAState): {
+                "question": ...,
+                "messages": ...,
+            }
+            llm        (BaseChatModel): LLM for answer node
+            template_name (str | None): Prompt Template name
+        Returns:
+            QAState: {
+                "answer": ...
+            }
+        """
+        if llm is None:
+            error_msg: str = 'LLM is not passed!'
+            raise RuntimeError(error_msg)
+
+        template = get_template(node_name='answer_node', template_name=template_name).partial(
+            messages=state.get_formatted_messages()
+        )
+        try:
+            chain: Runnable = (
+                {'question': RunnablePassthrough()}
+                | template
+                | llm.with_structured_output(AnswerNodeResponse, method='json_schema')
+            )
+        except NotImplementedError:
+            chain = (
+                {'question': RunnablePassthrough()}
+                | template
+                | llm
+                | PydanticOutputParser(pydantic_object=AnswerNodeResponse)
+            )
+
+        answer: AnswerNodeResponse = await chain.ainvoke(state.question)
+        return QAState.put_answer(answer=answer.answer)
